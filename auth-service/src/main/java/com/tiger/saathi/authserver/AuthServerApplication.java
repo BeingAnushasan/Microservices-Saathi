525f45531e8e99e08447f920204ab66021b58f21
beinganushasan <beinganushasan@gmail.com>
2020-06-05 23:15:39 +0545

    added Eureka server & works fine

--- auth-service/src/main/java/com/tiger/saathi/authserver/AuthServerApplication.java
+++ auth-service/src/main/java/com/tiger/saathi/authserver/AuthServerApplication.java
@@ -2,10 +2,12 @@ package com.tiger.saathi.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@EnableEurekaClient
 public class AuthServerApplication {
 
     public static void main(String[] args) {
